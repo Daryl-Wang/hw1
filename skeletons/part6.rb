02e8a4b682ba23388af1881ac97d7e349b7b4911
Daryl Wang <darylywang@gmail.com>
2012-09-02 15:58:23 -0700

    Commiting files on parts 6-7. Need to talk about parts 6b/c with GSIs.

--- skeletons/part6.rb
+++ skeletons/part6.rb
@@ -1,5 +1,5 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.0, :yen => 0.013, :yens => 0.013, :euro => 1.292, :euros => 1.292, :rupee => 0.019, :rupees => 0.019, :dollar => 1.0, :dollars => 1.0 }
   def method_missing(method_id)
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
@@ -8,12 +8,40 @@ class Numeric
       super
     end
   end
-end
-puts 44.yen()
-class String
-  # YOUR CODE HERE
+
+  def in( currency )
+    if @@currencies.has_key?( currency )
+      return self / @@currencies[ currency ]
+    end
+      
+  end
 end
 
-module Enumerable
+puts 44.yen
+puts 5.dollars.in( :euro )
+puts 10.euros.in( :rupees )
+
+require 'part1'
+class String
   # YOUR CODE HERE
+  def palindrome?
+    return Part1.palindrome?( self )
+  end
 end
+
+# CHECK IF THIS IS RIGHT
+puts "foo".palindrome?
+puts "Madam I'm Adam".palindrome?
+
+module Enumerable
+  def palindrome?
+    arr = self.to_a
+    arr.size do | index |
+      if arr[ index ] != arr[ arr.size - index - 1 ]
+        return false
+      end
+    end
+    return true
+  end
+end
+puts [1, 2, 3, 2, 1].palindrome?
--- skeletons/part7.rb
+++ skeletons/part7.rb
@@ -1,4 +1,19 @@
 class CartesianProduct
   include Enumerable
   # YOUR CODE HERE
+  def initialize( setA, setB )
+    @setA = setA
+    @setB = setB
+  end
+
+  def each
+    @setA.each do | a |
+      @setB.each do | b |
+        yield [ a, b ]
+      end
+    end
+  end
 end
+
+c = CartesianProduct.new([:a,:b], [4,5])
+c.each { |elt| puts elt.inspect }
