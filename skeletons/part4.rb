9debaab9fbf3e094b0b4d2758cdbfc4db32ba661
Daryl Wang <darylywang@gmail.com>
2012-09-06 20:30:28 -0700

    Fixed errors with part 4.

--- skeletons/part4.rb
+++ skeletons/part4.rb
@@ -1,11 +1,14 @@
 class Dessert
+  attr_accessor :name
+  attr_accessor :calories
+
   def initialize(name, calories)
     @name = name
     @calories = calories
   end
   
   def healthy?
-    return @calories > 200
+    return @calories < 200
   end
   
   def delicious?
@@ -14,6 +17,7 @@ class Dessert
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
   def initialize(name, calories, flavor)
     super( name, calories )
     @flavor = flavor
